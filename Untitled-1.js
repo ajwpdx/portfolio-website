d24472bc6ee494270f4800a62d9a73242c8e291a
Alex Williams <alexjwilliams1@gmail.com>
2020-12-10 11:56:36 -0800

    removed file

--- Untitled-1.js
+++
@@ -1,9 +0,0 @@
-// If you are gonna objectify me, let me help you out...
-const alexW = {
-"name": "Alex",
-"gender": "male",
-"age": 31,
-"hobbies": ["rock climbing", "running", "jazz", "making useless React apps"],
-"pets": {"chickens": ["Kwansa", "Naomi", "Jacqui"], "dogs": [], "cats": []},
-"status":"single ;)"
-}
